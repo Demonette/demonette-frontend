5527d0fbd42feaf75451274c76460a0aa9847adf
Simon Meoni <simonmeoni@aol.com>
2018-03-05 10:29:01 +0100

    add communication between front and backend - this close #2

---
+++ config/url-config.js
@@ -0,0 +1,4 @@
+export const url = {
+  production : 'https://localhost:3000',
+  development: 'http://localhost:3000'
+};
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -2,22 +2,50 @@
   <div class="search">
     <div class="panel-block">
       <p class="control has-icons-left">
-        <input class="input" type="text" placeholder="search">
+        <input class="input" type="text" placeholder="search" v-model="queryField">
         <span class="icon is-left">
             <i class="fas fa-search"></i>
           </span>
       </p>
     </div>
+    <p>{{ this.entry }}</p>
   </div>
 </template>
 
 <script>
-export default {
+  import axios from 'axios';
+  import _ from 'lodash';
+  import { url } from '../../config/url-config';
+
+  export default {
   name: 'Search',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      entry: '',
+      queryField: '',
     };
+  },
+  watch: {
+    queryField() {
+      this.entry = '...';
+      this.debounceQuery();
+    },
+  },
+  methods: {
+    query() {
+      if (!this.queryField) {
+        this.entry = '';
+      } else {
+        axios.get(`${url[process.env.NODE_ENV]}/search?token=${this.queryField}`)
+          .then((response) => {
+            this.entry = response.data;
+          })
+          .catch(() => {
+            this.entry = 'pas de résultat pour la recherche courante ...';
+          });
+      }
+    },
+    debounceQuery: _.debounce(this.query(), 300),
   },
 };
 </script>
