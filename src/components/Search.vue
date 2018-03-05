dd28c3e166560d662d5cc441d15c7ae8eada862b
Simon Meoni <simonmeoni@aol.com>
2018-03-05 14:26:39 +0100

    add media content for each element of return request

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -8,7 +8,39 @@
           </span>
       </p>
     </div>
-    <p>{{ this.entry }}</p>
+    <br/>
+    <div class="container is-fluid">
+      <div v-if="this.entry.length === 0">
+        <p class="has-text-centered is-size-5">aucun résultat pour la recherche courante ...</p>
+      </div>
+      <div v-else-if="this.entry === '...'">
+        <p class="has-text-centered is-size-4">{{ this.entry }}</p>
+      </div>
+      <div v-else>
+        <div class="box" v-for="el in entry" :key="el._id">
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong class="is-size-3">
+                  {{el._source.graphie_1}}/{{el._source.graphie_2}} ·
+                </strong>
+                <small class="is-size-4">{{el._source.type_1}}/{{el._source.type_2}} </small>
+                <br/>
+                {{ el }}
+              </p>
+              <table>
+                  <tbody>
+                    <tr v-for="(value, key) in el._source" :key="value">
+                      <td>{{ key }}</td>
+                      <td>{{ value }}</td>
+                    </tr>
+                  </tbody>
+                </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -45,7 +77,8 @@
           });
       }
     },
-    debounceQuery: _.debounce(this.query(), 300),
+    // eslint-disable-next-line func-names
+    debounceQuery: _.debounce(function () { this.query(); }, 300),
   },
 };
 </script>
