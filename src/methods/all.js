3b74b5ccab60a6e88a332dccf89bc6cea6ce6011
Simon Meoni <simonmeoni@aol.com>
2018-05-28 18:18:54 +0200

    write basic code for aggregate facet serach with similar fields property, the code is ugly & not used in the query methods : refactoring and some fixes is needed !

--- src/methods/all.js
+++ src/methods/all.js
@@ -2,10 +2,34 @@ import axios from 'axios';
 import { url } from '../../config/url-config';
 
 export default function autocomplete(querySize, queryFrom) {
+  function formatAgg(agg) {
+    Object.keys(agg).forEach((el) => {
+      if (el.includes('_')) {
+        const newName = el.slice(0, -2);
+        if (!(newName in agg)) {
+          agg[newName] = agg[el];
+        } else if (el.includes('count')) {
+          agg[newName].value += agg[el].value;
+        } else {
+          const dictAgg = {};
+          agg[newName].buckets.forEach((ela) => { dictAgg[ela.key] = ela; });
+          agg[el].buckets.forEach(eln => {
+            if (eln.key in dictAgg) {
+              eln.doc_count += dictAgg[eln.key].doc_count;
+            }
+          });
+          console.log(agg[el]);
+          agg[newName] = agg[el];
+        }
+        delete agg[el];
+      }
+    });
+    return agg;
+  }
   return axios.get(
     `${url[process.env.NODE_ENV]}/all?size=${querySize}&from=${queryFrom}`)
     .then(response => ({
       data: response.data.hits.hits,
       total: response.data.hits.total,
-      facet: response.data.aggregations }));
+      facet: formatAgg(response.data.aggregations) }));
 }
