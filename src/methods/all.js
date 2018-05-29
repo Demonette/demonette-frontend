c5ebfa0474258cf04e43f2c77a8faac743731bdd
Simon Meoni <simonmeoni@aol.com>
2018-05-29 11:09:10 +0200

    reformat code

--- src/methods/all.js
+++ src/methods/all.js
@@ -1,8 +1,23 @@
 import axios from 'axios';
 import { url } from '../../config/url-config';
 
+let agg = {};
+
 export default function autocomplete(querySize, queryFrom) {
-  function formatAgg(agg) {
+  function addNewKeyToAgg(newName, el) {
+    const dictAgg = {};
+    agg[newName].buckets.forEach((ela) => {
+      dictAgg[ela.key] = ela;
+    });
+    agg[el].buckets.forEach((eln, idx) => {
+      if (eln.key in dictAgg) {
+        agg[el].buckets[idx].doc_count += dictAgg[eln.key].doc_count;
+      }
+    });
+    agg[newName] = agg[el];
+  }
+
+  function formatAgg() {
     Object.keys(agg).forEach((el) => {
       if (el.includes('_')) {
         const newName = el.slice(0, -2);
@@ -11,25 +26,23 @@ export default function autocomplete(querySize, queryFrom) {
         } else if (el.includes('count')) {
           agg[newName].value += agg[el].value;
         } else {
-          const dictAgg = {};
-          agg[newName].buckets.forEach((ela) => { dictAgg[ela.key] = ela; });
-          agg[el].buckets.forEach(eln => {
-            if (eln.key in dictAgg) {
-              eln.doc_count += dictAgg[eln.key].doc_count;
-            }
-          });
-          console.log(agg[el]);
-          agg[newName] = agg[el];
+          addNewKeyToAgg(newName, el);
+          delete agg[el];
         }
-        delete agg[el];
       }
     });
     return agg;
   }
+
   return axios.get(
     `${url[process.env.NODE_ENV]}/all?size=${querySize}&from=${queryFrom}`)
-    .then(response => ({
-      data: response.data.hits.hits,
-      total: response.data.hits.total,
-      facet: formatAgg(response.data.aggregations) }));
+    .then((response) => {
+      agg = response.data.aggregations;
+      formatAgg();
+      return {
+        data: response.data.hits.hits,
+        total: response.data.hits.total,
+        facet: agg,
+      };
+    });
 }
