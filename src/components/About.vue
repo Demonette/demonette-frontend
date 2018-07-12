0290c23b06735fe27a84c8fcdae80016fc78978a
Simon Meoni <simonmeoni@aol.com>
2018-07-12 12:07:54 +0200

    rename logo and addd reponsive content

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="about">
-    <institution class="institution"/>
-    <institution class="institution"/>
-    <institution class="institution"/>
+    <div class="institution-wrapper">
+    <img src="../assets/cnrs.png"/>
+    <institution class="institution" name="CNRS" :content="cnrs"/>
+    </div>
+    <div class="institution-wrapper">
+    <img src="../assets/atilf.gif"/>
+    <institution class="institution" name="ATILF" :content="atilf"/>
+    </div>
+    <div class="institution-wrapper">
+    <img src="../assets/ul.jpg"/>
+    <institution name="Université de Lorraine"
+                :content="udl"/>
+    </div>
   </div>
 </template>
 
@@ -16,6 +26,18 @@ export default {
   data() {
     return {
       appVersion: version[process.env.NODE_ENV],
+      cnrs: `Le Centre national de la recherche scientifique, plus connu sous le sigle CNRS,
+      est le plus grand organisme public français de recherche scientifique.
+      Juridiquement, c'est un établissement public à caractère scientifique et technologique (EPST) placé
+      sous la tutelle administrative du ministère de l'Éducation nationale, de l'Enseignement supérieur et de la Recherche.`,
+      atilf: ` L'UMR 7118 ATILF (CNRS/Université de Lorraine) voit son excellence reconnue tant au plan national qu’international.
+      Rattachée à l’Institut des sciences humaines et sociales du CNRS,
+      elle est aussi laboratoire d’interface avec le Département des sciences et technologie de l’information et de l’ingénierie.
+      Elle est membre de la fédération CNRS - Institut de Linguistique Française
+      et du consortium international TEI (Text Encoding Initiative).`,
+      udl: `Université de tous les savoirs, l’Université de Lorraine promeut la mutualisation des savoirs.
+      Des sciences fondamentales aux sciences humaines, elle crée des éco-systèmes trans-disciplinaires au service de l’innovation,
+      qui accélèrent le passage de la connaissance aux applications.`,
     };
   },
 };
@@ -24,6 +46,20 @@ export default {
   .about {
     display: flex;
     flex-direction: column;
-    justify-content: space-around;
+    justify-content: space-evenly;
+  }
+  .institution-wrapper {
+    display: flex;
+    flex-direction: row;
+    margin-left: 10%;
+  }
+  .institution {
+    flex-basis: 50%;
+  }
+  img {
+    width: 10%;
+    height: 10%;
+    flex-basis: 1%;
+    margin-right: 2%;
   }
 </style>
--- src/components/Institution.vue
+++ src/components/Institution.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="institution">
-      <h1 class="is-size-3">CNRS</h1>
-    <p>
-      Le Centre national de la recherche scientifique, plus connu sous le sigle CNRS,
-      est le plus grand organisme public français de recherche scientifique.
-      Juridiquement, c'est un établissement public à caractère scientifique et technologique (EPST)
-      placé sous la tutelle administrative du ministère de l'Éducation nationale,
-      de l'Enseignement supérieur et de la Recherche.
-    </p>
+    <h1 class="is-size-3">{{ name }}</h1>
+    <p class="has-text-justified">{{ content }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'institution',
+  props: ['name', 'content'],
 };
+
 </script>
